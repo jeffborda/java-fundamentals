384a17efcf132f1e471765a8ac8ae35bdb09b549
Jeff Borda <jeffborda@outlook.com>
2018-11-27 09:57:59 -0800

    pluralize complete

--- src/main/java/basics/Main.java
+++ src/main/java/basics/Main.java
@@ -2,6 +2,25 @@ package basics;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("thank God");
+
+        // Pluralize Tests
+        int dogCount = 1;
+        System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
+
+        int catCount = 2;
+        System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
+
+        int turtleCount = 0;
+        System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
     }
+
+    public static String pluralize(String inputWord, int inputNumber) {
+
+        if(inputNumber == 0 || inputNumber > 1) {
+            return inputWord + "s";
+        }
+
+        return inputWord;
+    }
+
 }
