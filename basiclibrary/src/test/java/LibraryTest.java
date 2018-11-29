fb53670877d7f10a9f7b53d316b59e204aabc063
Jeff Borda <jeffborda@outlook.com>
2018-11-29 10:32:56 -0800

    finished weather data problem, need to test better

--- basiclibrary/src/test/java/LibraryTest.java
+++ basiclibrary/src/test/java/LibraryTest.java
@@ -58,4 +58,18 @@ public class LibraryTest {
 
         assertArrayEquals("calculateAverageOfArrays should return array with lowest avg inside a 2D array", expectedArray, classUnderTest.calculateAverageOfArrays(average1) );
     }
+
+    @Test
+    public void testAnalyzeWeatherData() {
+        Library classUnderTest = new Library();
+
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+
+        classUnderTest.analyzeWeatherData(weeklyMonthTemperatures);
+    }
 }
