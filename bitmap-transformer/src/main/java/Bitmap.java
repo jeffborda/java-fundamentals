c5b900a69ee2c975a6de5bba28fefbc035a20a7d
Jeff Borda <jeffborda@outlook.com>
2018-12-02 08:58:23 -0800

    added gitignore

--- bitmap-transformer/src/main/java/Bitmap.java
+++ bitmap-transformer/src/main/java/Bitmap.java
@@ -2,6 +2,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +13,7 @@ public class Bitmap {
     private String output;
     private String transform;
     BufferedImage bmp = null;
-    List<List<Integer>> bmpMatrix = new ArrayList<List<Integer>>();
+
 
 
     public Bitmap(String input, String output, String transform) {
@@ -22,15 +23,19 @@ public class Bitmap {
         this.output = output;
         this.transform = transform;
 
+        Path inputPath = FileSystems.getDefault().getPath(input);
+
 
         try {
-            bmp = ImageIO.read(new File(input)); //"resources/smiley.bmp"
+            //bmp = ImageIO.read(new File(input)); //"resources/smiley.bmp"
 
             for(int i = 0; i < bmp.getHeight(); i++) {
                 for(int j = 0; j < bmp.getWidth(); j++) {
                     System.out.println(bmp.getRGB(j, i));
                 }
             }
+
+            this.bmp = ImageIO.read(inputPath.toFile());
 
         } catch (IOException e) {
             System.out.println(e);
@@ -41,6 +46,11 @@ public class Bitmap {
     public void flip() {
         //read in file
         //save
+    }
+
+    public boolean save() {
+
+        return false;
     }
 
 }
