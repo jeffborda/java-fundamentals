d5a01bc97be2fe8b90bdbd3714211318c3ac3b5e
Jeff Borda <jeffborda@outlook.com>
2018-12-04 10:37:42 -0800

    developing

---
+++ inheritance/src/test/java/ReviewTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReviewTest {
+
+    @Test
+    public void testToString() {
+        Review bobsReview = new Review("Bob's Burgers was really good, and they have wonderful service.", "Dan", 4);
+        String reviewBody = "Bob's Burgers was really good, and they have wonderful service.";
+        assertEquals("Should return string with stars, then body, then author.", "**** " + reviewBody + " Written by: " + "Dan.", bobsReview.toString());
+
+        Review toshisReview = new Review("Toshi's is my favorite teriaki restaurant.", "Sooz", 5);
+        assertEquals("Should return string with stars, then body, then author.", "***** Toshi's is my favorite teriaki restaurant. Written by: Sooz.", toshisReview.toString());
+
+    }
+}
